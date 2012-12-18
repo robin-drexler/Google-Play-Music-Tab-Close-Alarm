7155dae48ede53761ca13b317164c1baaf590982
Robin Drexler <drexler.robin@gmail.com>
2012-12-18 16:22:44 +0100

    implement feature, add stock android icon :)

--- js/content_script.js
+++ js/content_script.js
@@ -1,3 +1,8 @@
-$(function() {
-    alert('Yay, I\'m an extension :)');
-});
+(function() {
+    window.onbeforeunload = function() {
+        var isMusicRunning =  document.querySelector('#playPause').getAttribute('aria-pressed') === "true",
+            leaveMessage = 'Hey, you\'re playing music.'
+
+        return isMusicRunning ? leaveMessage : null;
+    }
+})()

30da599911e16f8d0e553f329b3164fe9c44b88f
Robin Drexler <drexler.robin@gmail.com>
2014-07-01 19:00:30 +0200

    add tracking to ensure functionality with metrics monitoring later on

--- js/content_script.js
+++ js/content_script.js
@@ -1,8 +1,14 @@
-(function() {
-    window.onbeforeunload = function() {
-        var isMusicRunning =  document.querySelector('[data-id=play-pause]').getAttribute('title') == 'Pause',
-            leaveMessage = 'Hey, you\'re playing music.'
+(function () {
+  window.onbeforeunload = function () {
+    var isMusicRunning = document.querySelector('[data-id=play-pause]').getAttribute('title') == 'Pause',
+      leaveMessage = 'Hey, you\'re playing music.';
 
-        return isMusicRunning ? leaveMessage : null;
+    chrome.runtime.sendMessage({type: 'beforeunload'});
+
+    if (isMusicRunning) {
+      chrome.runtime.sendMessage({type: 'prompted'});
     }
-})()
+
+    return isMusicRunning ? leaveMessage : null;
+  };
+})();
